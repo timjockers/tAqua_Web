ff4c89c4f00d4547e7f62c94db8a3764eda5b46a
Tim Jockers <tim.jockers@icloud.com>
2026-09-13 07:54:42 +0200

    Implemented fileexchange

--- fileexchange.cpp
+++ fileexchange.cpp
@@ -1,1 +1,39 @@
 #include "fileexchange.hpp"
+
+#include <iostream>
+using namespace libconfig;
+using namespace std;
+
+
+ConfigManager::ConfigManager(const string& filepath)
+    : path(filepath)
+{
+    updateConfig();
+}
+
+void ConfigManager::updateConfig()
+{
+    read();
+    store();
+}
+
+void ConfigManager::read()
+{
+    try
+    {
+        cfg.readFile(path.c_str());
+    }
+    catch(const FileIOException &fioex)
+    {
+        cerr << "I/O error while reading file." << endl;
+    }
+    catch(const ParseException &pex)
+    {
+        cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+                << " - " << pex.getError() << std::endl;
+    }
+}
+
+void ConfigManager::store()
+{
+}
--- fileexchange.hpp
+++ fileexchange.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <string>
+#include <libconfig.h++>
+
+
+class ConfigManager {
+public:
+    ConfigManager(const std::string& filepath);
+
+    void updateConfig();
+    
+private:
+    void read();
+    void store();
+
+    const std::string path;
+
+    libconfig::Config cfg;
+};
