5b4d6aad343b340bcb6e105816607a2a18532794
Tim Jockers <tim.jockers@icloud.com>
2026-09-14 13:49:17 +0200

    Redefined events, using struct instead of class

--- events.hpp
+++ events.hpp
@@ -1,42 +1,11 @@
 #pragma once
 
 #include <chrono>
+#include "types.hpp"
 
-class irrigationEvent{};
-
-
-class relayEvent : public irrigationEvent {
-public:
-    relayEvent(Relay r);
-
-    Relay getRelay() const;
-
-protected:
+struct scheduledEvent {
     Relay relay;
-};
-
-
-class durationEvent : public relayEvent {
-public:
-    durationEvent(Relay r, std::chrono::seconds irrDuration);
-
-protected:
     std::chrono::seconds duration;
-    std::chrono::system_clock::time_point startTime;
-};
-
-
-class buttonEvent : public durationEvent {
-public:
-    using durationEvent::durationEvent;
-};
-
-
-class scheduledEvent : public durationEvent {
-public:
-    scheduledEvent(Relay r, std::chrono::seconds irrDuration, Weekday weekday, std::chrono::minutes startTime);
-
-private:
-    Weekday wday;
-    std::chrono::minutes starttime; // Minutes past midnight
+    Weekday weekday;
+    std::chrono::minutes startTime;
 };
