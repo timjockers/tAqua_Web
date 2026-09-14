ba31e5363976416d3a41743801c6ffbcee9e4dbf
Tim Jockers <tim.jockers@icloud.com>
2026-09-14 12:39:28 +0200

    Added weekdays

--- types.hpp
+++ types.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+// Weekdays
+enum class Weekday {
+    Sunday = 0,
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6
+};
+
 // The 3 possible relay configurations
 enum class RelayConfig {
     UNUSED = 0,
