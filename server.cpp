925891172e820cb4aff7f688709b4839eb06b47e
Tim Jockers <tim.jockers@icloud.com>
2026-09-15 07:48:48 +0200

    Added function to read html file

--- server.cpp
+++ server.cpp
@@ -1,1 +1,16 @@
 #include "server.hpp"
+
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+static string read_html_file(const string& path) {
+    ifstream file(path);
+    if (!file.is_open()) {
+        return "";
+    }
+    stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
