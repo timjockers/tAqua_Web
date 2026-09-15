58f574b4f9a3fa1380c1fc0dbe4e199d0c169f81
Tim Jockers <tim.jockers@icloud.com>
2026-09-15 07:47:39 +0200

    Declared HTTPServer structure

--- server.hpp
+++ server.hpp
@@ -1,3 +1,17 @@
 #pragma once
 
+#include <string>
 #include "httplib.h"
+#include "fileexchange.hpp"
+
+class HTTPServer {
+public:
+    HTTPServer(ConfigManager* configManager);
+    void start(const std::string& host, int port);
+
+private:
+    httplib::Server svr;
+    ConfigManager* configM;
+    void setupWebpage();
+    void setupRoutes();
+};
