d4c1aadab8feb1c9812dd9266815ede108fc3958
Tim Jockers <tim.jockers@icloud.com>
2026-09-16 19:19:57 +0200

    First version of main js functions

--- static/script.js
+++ static/script.js
@@ -1,6 +1,41 @@
+// OVERLAY
+const overlay = document.querySelector('.overlay');
+overlay.addEventListener('click', closeAllPopup);
+
+// SETTINGS-POPUP
+const settingsButton = document.querySelector('.settings-button-container');
+const settingsPopup = document.querySelector('.settings-popup');
+
+settingsPopup.querySelector('.popup-close-button').addEventListener('click', closeAllPopup);
+settingsButton.addEventListener('click', function(event) { openPopup(settingsPopup); });
+
+// RELAIS-CONFIGURATION
 function loadConfig() {
-    getJSON('/api/relayConfig').then(conf => {
-        document.getElementById("rc").textContent = JSON.stringify(conf);
+    getJSON('/api/configure').then(conf => {
+        const valveBtns = document.querySelectorAll('.settings-valve');
+        console.log(conf);
+        for (let v = 0; v < 8; v++) {
+            valveBtns.item(v).dataset.state = conf[v];
+        }
     });
 }
 loadConfig();
+
+document.querySelectorAll('.settings-valve').forEach(valve => {
+    valve.addEventListener('click', () => {
+        const currentState = valve.getAttribute('data-state');
+
+        s = "0";
+        if (currentState === "0") {
+            valve.setAttribute('data-state', "1");
+            s = "1";
+        } else if (currentState === "1") {
+            valve.setAttribute('data-state', "2");
+            s = "2";
+        } else {
+            valve.setAttribute('data-state', "0");
+        }
+        
+        writeJSON("/api/configure", "{\"id\": " + valve.getAttribute('data-id') + ", \"status\": " + s + "}");
+    });
+});
