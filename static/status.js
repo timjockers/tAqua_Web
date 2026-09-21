d2ec28a4001beec5a3486518360241e295faeaeb
Tim Jockers <tim.jockers@icloud.com>
2026-09-21 19:50:29 +0200

    Added javascript for popup cards visibility

--- static/status.js
+++ static/status.js
@@ -1,13 +1,19 @@
 const statusContainer = document.querySelector(".bottom-status");
+const scheduledPopupWrapper = document.querySelector(".popup-wrapper-scheduled-popup");
 
 function updateStatusHTML() {
     getJSON('/api/relayConfig').then(conf => {
-        const statusElements = document.querySelectorAll('.valve-status');
+        const statusElements = statusContainer.querySelectorAll('.valve-status');
+        
+        const scheduledPElements = scheduledPopupWrapper.querySelectorAll('.popup-content');
+
         for (let v = 0; v < 8; v++) {
             if (conf[v] == 1) {
                 statusElements.item(v).classList.remove("hidden");
+                scheduledPElements.item(v).classList.remove("hidden");
             } else {
                 statusElements.item(v).classList.add("hidden");
+                scheduledPElements.item(v).classList.add("hidden");
             }
         }
     });
