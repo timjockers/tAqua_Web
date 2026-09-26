1b1ded70ce2c509675393b46c77d7d40f6371a4f
Tim Jockers <tim.jockers@icloud.com>
2026-09-26 10:58:23 +0200

    Added iconBoxClicked function

--- static/scheduled.js
+++ static/scheduled.js
@@ -1,7 +1,18 @@
-const iconBoxes = document.querySelectorAll('.icon-box');
-
-iconBoxes.forEach(box => {
-    box.addEventListener('click', (event) => {
-        console.log('Button clicked:', event.target);
-    });
+document.addEventListener('click', (event) => {
+    if (event.target.matches('.icon-box')) {
+        iconBoxClicked(event);
+    }
 });
+
+function iconBoxClicked(event) {
+    const isAlreadyExpanded = clickedBox.classList.contains('expanded');
+
+    const expandedBoxes = document.querySelectorAll('.icon-box.expanded');
+    expandedBoxes.forEach(box => {
+        box.classList.remove('expanded');
+    });
+
+    if (!isAlreadyExpanded) {
+        clickedBox.classList.add('expanded');
+    }
+}
